ffec17c9029dcaeb0e178618acb5ebfec340b0d6
khwiri <5530052+khwiri@users.noreply.github.com>
2026-05-22 21:45:07 -0400

    chore: add deps and update project configs

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/main.ts"],
+  format: ["esm", "cjs"],
+  target: "node24",
+  platform: "node",
+  outDir: "dist",
+  clean: true,
+  sourcemap: true,
+  dts: false,
+  shims: false,
+  splitting: false,
+  minify: false,
+  banner: {
+    js: "#!/usr/bin/env node",
+  },
+});
